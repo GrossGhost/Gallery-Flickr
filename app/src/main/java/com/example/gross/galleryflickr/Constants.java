80b76cc33c55e79a36b9d1aaddb9afe3a1caacca
GrossGhost <grossghost13@gmail.com>
2017-04-15 22:45:42 +0300

    new commit

--- app/src/main/java/com/example/gross/galleryflickr/Constants.java
+++ app/src/main/java/com/example/gross/galleryflickr/Constants.java
@@ -3,7 +3,7 @@ package com.example.gross.galleryflickr;
 
 public class Constants {
 
-    //commit
+    //commit 2
     public static final String BASE_URL = "https://api.flickr.com";
     public static final String API_KEY = "591d8ca3f04452ae0b6325b1bfe9dfab";
     public static final String METHOD_INTERESTINGNESS = "flickr.interestingness.getList";
